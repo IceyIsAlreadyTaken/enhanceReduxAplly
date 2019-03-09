7663efb08014de426ce6aae5dbf12343c68c23c1
jiaoyingying <jiaoyingying@xylink.com>
2019-03-09 23:06:16 +0800

    docs: remove blank space

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-
-
 class Counter extends Component {
-
   render() {
-
-
     return (
       <div>
         <p>clicked {this.context.value} times</p>
